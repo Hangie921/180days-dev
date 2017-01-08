51b4dc60981c89eb6becd42d5e8a2d1212aba0c5
hangie921 <hangie921@gmail.com>
2017-01-08 21:48:37 +0800

    add files

---
+++ src/pages/005Date_tracker.vue
@@ -0,0 +1,135 @@
+<template lang="jade">
+  .container
+    .back
+    ul#week
+      li.weekdays#sun
+        | Sun
+      li.weekdays#mon
+        | Mon
+      li.weekdays#tue
+        | Tue
+      li.weekdays#wed
+        | Wed
+      li.weekdays#thr
+        | Thr
+      li.weekdays#fri
+        | Fri
+      li.weekdays#sat
+        | Sat
+    .weekMessage
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+  data () {
+    return {
+    }
+  },
+  mounted () {
+    let date = new Date()
+    let day = date.getDay()
+    let weekday = [
+      `Oh!My! It's Sunday!!`,
+      `Oh no!!! Today is a blue Monday`,
+      `Tuesday!! Keep going!`,
+      `Wednesday is the midday of the weekdays.`,
+      `Tuesday,two days more to the weekend!!`,
+      `Let's party!! It's Friday!!`,
+      `Where are ya goin' on Saturday night?`
+    ]
+    let speech = weekday[day]
+    switch (day) {
+      case 0:
+        day = '#sun'
+        break
+      case 1:
+        day = '#mon'
+        break
+      case 2:
+        day = '#tue'
+        break
+      case 3:
+        day = '#wed'
+        break
+      case 4:
+        day = '#thr'
+        break
+      case 5:
+        day = '#fri'
+        break
+      case 6:
+        day = '#sat'
+        break
+    }
+    $(day).css({
+      'background-color': '#33aa00'
+    })
+    .css({
+      'color': '#fff',
+      'font-size': '40px',
+      'height': '300px',
+      'text-shadow': '-2px -2px 1px #555'
+    })
+    $('.weekMessage').text(speech)
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '~sass/_varaibles.scss';
+@import '~sass/_breakpoint.scss';
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+}
+
+.weekdays {
+  display: inline-block;
+  box-sizing: border-box;
+  width: 13.2757143%;
+  /*background-color:#F1C40F;*/
+  
+  color: #000;
+  height: 100px;
+  text-align: center;
+  padding-top: 40px;
+  font-size: 20px;
+  position: relative;
+}
+
+.back {
+  position: absolute;
+  width: 100%;
+  height: 120px;
+  margin: auto;
+  background-color: #8AE62E;
+}
+
+.weekMessage {
+  width: 100%;
+  height: 150px;
+  background-color: #33aa00;
+  box-sizing: border-box;
+  position: absolute;
+  top: 300px;
+  font-size: 40px;
+  line-height: 40px;
+  text-align: center;
+  color: #fff;
+  padding: 50px;
+  text-shadow: -2px -2px 1px #555;
+}
+
+</style>
---
+++ src/pages/006Technical_boxes.vue
@@ -0,0 +1,80 @@
+<template lang="jade">
+  .container
+    .row.clearfix(v-for="n in 10")
+      a.box.clearfix(v-for="c in 10" v-on:mouseenter="hover($event)")
+</template>
+
+<script>
+import { getRand } from '../utils.js'
+import $ from 'jquery'
+
+export default {
+  data () {
+    return {
+      a: '臣亮言：先帝創業未半，而中道崩殂。今天下三分，益州疲弊，此誠危急存亡之秋也。然侍衛之臣，不懈於內；忠志之士，忘身於外者，蓋追先帝之殊遇，欲報之於陛下也。誠宜開張聖聽，以光先帝遺德，恢弘志士之氣；不宜妄自菲薄，引喻失義，以塞忠諫之路也。宮中府中，俱為一體，陟罰臧否，不宜異同。若有作姦犯科，及為忠善者，宜付有司，論其刑賞，以昭陛下平明之治，不宜篇私，使內外異法也。',
+      counter: 0
+    }
+  },
+  computed: {
+    ary () {
+      let temp = []
+      for (let i = 0; i < this.a.length; i++) {
+        temp[i] = this.a.substr(i, 1)
+      }
+      return temp
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    hover (e) {
+      let random = 'rgb('.concat(getRand(0, 255), ',', getRand(0, 255), ',', getRand(0, 255), ')')
+      $(e.target).css('backgroundColor', random).text(this.ary[this.counter])
+      if (this.counter !== 181) {
+        this.counter++
+      } else {
+        this.counter = 0
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import '~sass/_varaibles.scss';
+@import '~sass/_breakpoint.scss';
+div{
+  box-sizing: border-box;
+}
+.container{
+  width: 100%;
+  height: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color:#000;
+}
+.box{
+  display:block;
+  text-decoration: none;
+  width: 10%;
+  height: 100px;
+  z-index:1;
+  float:left;
+  position: relative;
+  color:#111;
+  text-shadow: 1px 1px 0px #aaa,
+         2px 2px 0px #aaa,
+         3px 3px 0px #aaa;
+  font-size:30px;
+  line-height:30px;
+  text-align:center;
+  font-family:'courier';
+  padding-top:30px;
+  &:hover{
+    box-shadow: #fff 0px 0px 8px;
+    z-index: 10000;
+  }
+}
+</style>
